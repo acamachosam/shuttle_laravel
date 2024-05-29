78066098bc38724003127c04e6537bfad3423b87
AlbertoCamachoSampedro <albertocamachosampedro@gmail.com>
2024-05-29 13:43:39 +0200

    Homepage created, added images, created new routes for the rest of the pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function(){
+    return "Login";
+});
+
+Route::get('/register', function(){
+    return "Register";
+});
+
+Route::get('/dashboard', function () {
+    return "Panel de control";
+});
